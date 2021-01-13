ed0189910810bff9a9151dea8edc3e200cee7734
Ewout ter Hoeven <E.M.terHoeven@student.tudelft.nl>
2021-01-13 15:46:48 +0100

    Try using cargo-c and cinstall

--- ext/rav1e.cmd
+++ ext/rav1e.cmd
@@ -14,8 +14,8 @@
 git clone -b 0.4 --depth 1 https://github.com/xiph/rav1e.git
 
 cd rav1e
-cargo install --force cbindgen
-cbindgen -c cbindgen.toml -l C -o target/release/include/rav1e/rav1e.h --crate rav1e .
+cargo install cargo-c
+cargo cinstall --release
 
 cargo build --lib --release --features capi
 cd ..
